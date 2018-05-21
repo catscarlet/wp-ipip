9b726aae685d24f2fdf85e5137534e61a56e2c83
catscarlet <i@catscarlet.com>
2018-05-21 22:37:05 +0800

    Minor adjust

--- wp-ipip-options.php
+++ wp-ipip-options.php
@@ -1,20 +1,21 @@
 <?php
 
-$tplpath = WP_PLUGIN_DIR.'/wp-ipip/wp-ipip-options.html';
+$tpl_path = WP_PLUGIN_DIR.'/wp-ipip/wp-ipip-options.html';
 
-if (!is_readable($tplpath)) {
+if (!is_readable($tpl_path)) {
     echo 'Cannot read file wp-ipip-options.html at '.getcwd();
 
     return;
 }
-$tpl = file_get_contents($tplpath);
+
+$tpl = file_get_contents($tpl_path);
 
 $ipip_data_filepath = WP_PLUGIN_DIR.'/wp-ipip/17mon/php/17monipdb.dat';
 
 if (is_readable($ipip_data_filepath)) {
-    $ipip_data_status = '<span style="color: green;">检测到IPIP数据库文件</span>';
+    $ipip_data_status = '<span style="color: green;">检测到 IPIP.net 数据库文件</span>';
 } else {
-    $ipip_data_status = '<span style="color: red;">未检测到IPIP数据库文件</span> 于 <code>'.$ipip_data_filepath.'</code>';
+    $ipip_data_status = '<span style="color: red;">未检测到 IPIP.net 数据库文件</span> 于 <code>'.$ipip_data_filepath.'</code>';
 }
 
 $search = ['{{$ipip_data_status}}'];
--- wp-ipip.php
+++ wp-ipip.php
@@ -26,7 +26,7 @@ function wpipip_add_option_page()
 
 function load_wp_ipip_resources($hook_suffix)
 {
-    if ($hook_suffix == 'edit-comments.php') {
+    if ('edit-comments.php' == $hook_suffix) {
         wp_register_script('wp-ipip.js', plugin_dir_url(__FILE__).'js/wp-ipip.js', array('jquery'));
         wp_enqueue_script('wp-ipip.js');
         include_once WP_PLUGIN_DIR.'/wp-ipip/17mon/php/IP.class.php';
@@ -44,20 +44,20 @@ function wp_ipip($comment_text, $comment = null)
         $results = IP::find($comment->comment_author_IP);
     } catch (Exception $e) {
         $location = 'WP-IPIP Caught exception: '.$e->getMessage();
-        $wpipip_e = '<div class="wp-ipip-comment" display="none" id="wp_ipip_perfix_'.$comment->comment_ID.'" style="color:red">'.$location.'</div>';
+        $wpipip_e = '<div class="wp-ipip-comment" display="none" id="wp-ipip-perfix-'.$comment->comment_ID.'" style="color:red">'.$location.'</div>';
 
         echo $wpipip_e;
 
         return $comment_text;
     }
 
-    if ($results == 'N/A') {
+    if ('N/A' == $results) {
         return $comment_text;
     }
 
     $location = array();
     foreach ($results as $str) {
-        if ($str != null) {
+        if (null != $str) {
             $location[] = $str;
         }
     }
